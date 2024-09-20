695445e9df143a01e98e375d0e95ca6fb5e44394
ali72-20 <as20201118@gmail.com>
2024-09-20 14:57:54 +0300

    fix no ui appear bug and call product list view in home body widget

--- lib/fetures/home_screen/presentaion_layer/widgets/home_screen_body.dart
+++ lib/fetures/home_screen/presentaion_layer/widgets/home_screen_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:shop_app/fetures/home_screen/presentaion_layer/widgets/product_list_view.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const ProductListView();
   }
 }
