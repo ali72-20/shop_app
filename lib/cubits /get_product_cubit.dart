33f3ba474e274a7e6aae9bab11fe4d083a7c32f4
ali72-20 <as20201118@gmail.com>
2024-09-20 16:26:03 +0300

    finish add product cubit

---
+++ lib/cubits /get_product_cubit.dart
@@ -0,0 +1,19 @@
+
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/cubits%20/get_prodcut_state.dart';
+import 'package:shop_app/fetures/home_screen/data_layer/apis/product_api.dart';
+import 'package:shop_app/fetures/home_screen/data_layer/models/product_model.dart';
+
+class GetProductCubit extends Cubit<GetProductState>{
+  GetProductCubit():super(ProductsLoading());
+
+  getProducts() async{
+    try{
+      List<ProductModel>? product = await ProductApi(Dio()).getProducts();
+      emit(ProductsLoaded());
+    }catch(e){
+      emit(ProductsFailed());
+    }
+  }
+}
