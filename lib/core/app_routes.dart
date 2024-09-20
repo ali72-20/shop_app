b054cc4b33181dcbcb061519e0e97d60acd2910d
ali72-20 <as20201118@gmail.com>
2024-09-20 14:36:06 +0300

    add app routes

---
+++ lib/core/app_routes.dart
@@ -0,0 +1,15 @@
+import 'package:go_router/go_router.dart';
+import 'package:shop_app/fetures/home_screen/presentaion_layer/home_screen.dart';
+
+abstract class AppRoutes  {
+  static const kStartPath = '/';
+
+  static  final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: kStartPath,
+        builder: (context,state)=> const HomeScreen()
+      )
+    ]
+  );
+}
